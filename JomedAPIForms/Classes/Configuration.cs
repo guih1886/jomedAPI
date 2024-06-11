78da6686ecafb5ff35491187a9cd5f089fdef5f7
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-10 23:06:05 -0300

    Criação da classe de configuração
    Criação da classe de configuração

---
+++ JomedAPIForms/Classes/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace JomedAPIForms.Classes;
+
+public class Configuration
+{
+    public static IConfigurationRoot GetIConfiguration()
+    {
+        IConfigurationBuilder builder = new ConfigurationBuilder()
+        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        return builder.Build();
+    }
+}
