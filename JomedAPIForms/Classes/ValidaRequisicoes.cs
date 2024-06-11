830eecf1c720417daba2e9d251df75c4ccb02e55
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-11 15:36:46 -0300

    Criação das validações das requisições
    Criação das validações das requisições

---
+++ JomedAPIForms/Classes/ValidaRequisicoes.cs
@@ -0,0 +1,28 @@
+﻿
+namespace JomedAPIForms.Classes;
+
+public static class ValidaRequisicoes
+{
+    public static async Task<string> ValidaLoginReq(HttpResponseMessage resposta)
+    {
+        string msg = await resposta.Content.ReadAsStringAsync() ;
+        if (msg.Contains("E-mail não pode estar vazio.")) return "E-mail não pode estar vazio.";
+        if (msg.Contains("E-mail inválido.")) return "E-mail inválido.";
+        if (msg.Contains("Senha não pode estar vazio.")) return "Senha não pode estar vazio.";
+        if (msg.Contains("Usuário não encontrado.")) return "E-mail não cadastrado.";
+        if (msg.Contains("Senha incorreta.")) return "Senha inválida.";
+        return "Ok";
+    }
+
+    public static async Task<string> ValidaCadastroUsuarioReq(HttpResponseMessage resposta)
+    {
+        string msg = await resposta.Content.ReadAsStringAsync();
+        if (msg.Contains("O e-mail deve ser informado.")) return "O e-mail deve ser informado.";
+        if (msg.Contains("E-mail inválido.")) return "E-mail inválido.";
+        if (msg.Contains("A senha deve ser informada.")) return "A senha deve ser informada.";
+        if (msg.Contains("A confirmação de senha deve ser informada.")) return "A confirmação de senha deve ser informada.";
+        if (msg.Contains("As senhas não são iguais.")) return "As senhas não são iguais.";
+        if (msg.Contains("Usuário já cadastrado.")) return "Usuário já cadastrado.";
+        return "Ok";
+    }
+}
