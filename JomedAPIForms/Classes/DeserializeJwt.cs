1a7451a60b30fee92ec3bc1370543101e6c2d6d9
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-11 17:40:26 -0300

    Criado a classe para retornar as claims do jwt
    Criado a classe para retornar as claims do jwt

---
+++ JomedAPIForms/Classes/DeserializeJwt.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JomedAPIForms.Classes;
+
+public static class DeserializeJwt
+{
+    public static ClaimsPrincipal? JwtClaims(string token, string key)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        TokenValidationParameters tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        };
+        try
+        {
+            SecurityToken securityToken;
+            ClaimsPrincipal principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            return principal;
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+}
