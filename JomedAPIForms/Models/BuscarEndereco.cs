fee8a348f6ca47b6a9e5bb277f6b654055f50cf3
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-12 20:36:40 -0300

    Criado modelo para busca de cep
    Criado modelo para busca de cep

---
+++ JomedAPIForms/Models/BuscarEndereco.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JomedAPIForms.Models;
+
+public class BuscarEndereco
+{
+    [JsonPropertyName("logradouro")]
+    public string Logradouro { get; set; }
+
+    [JsonPropertyName("bairro")]
+    public string Bairro { get; set; }
+
+    [JsonPropertyName("cep")]
+    public string Cep { get; set; }
+
+    [JsonPropertyName("localidade")]
+    public string Localidade { get; set; }
+
+    [JsonPropertyName("uf")]
+    public string Uf { get; set; }
+
+    [JsonPropertyName("complemento")]
+    public string Complemento { get; set; }
+}
