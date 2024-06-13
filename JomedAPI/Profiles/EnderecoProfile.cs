aea951ceca2409d700e0fcce53dc969971d60a38
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-13 17:48:54 -0300

    Update EnderecoProfile.cs

--- JomedAPI/Profiles/EnderecoProfile.cs
+++ JomedAPI/Profiles/EnderecoProfile.cs
@@ -8,6 +8,6 @@ public class EnderecoProfile : Profile
 {
     public EnderecoProfile()
     {
-        CreateMap<UpdateEnderecoDto, Endereco>().ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+        CreateMap<UpdateEnderecoDto, Endereco>().ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null && !(srcMember is string str && string.IsNullOrWhiteSpace(str))));
     }
 }
