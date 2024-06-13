b9d7c6904dce01df5aa78f38e62e4fecde9d8592
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-13 14:54:31 -0300

    Update BuscarEndereco.cs

--- JomedAPIForms/Models/BuscarEndereco.cs
+++ JomedAPIForms/Models/BuscarEndereco.cs
@@ -5,20 +5,20 @@ namespace JomedAPIForms.Models;
 public class BuscarEndereco
 {
     [JsonPropertyName("logradouro")]
-    public string Logradouro { get; set; }
+    public string? Logradouro { get; set; }
 
     [JsonPropertyName("bairro")]
-    public string Bairro { get; set; }
+    public string? Bairro { get; set; }
 
     [JsonPropertyName("cep")]
-    public string Cep { get; set; }
+    public string? Cep { get; set; }
 
     [JsonPropertyName("localidade")]
-    public string Localidade { get; set; }
+    public string? Localidade { get; set; }
 
     [JsonPropertyName("uf")]
-    public string Uf { get; set; }
+    public string? Uf { get; set; }
 
     [JsonPropertyName("complemento")]
-    public string Complemento { get; set; }
+    public string? Complemento { get; set; }
 }
