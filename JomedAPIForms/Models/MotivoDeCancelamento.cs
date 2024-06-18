b4ed1d9d549ed5914252fe3cb84afe6d3ce1fd9e
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-18 12:44:25 -0300

    Criado tela de cancelamento de consultas
    Criado tela de cancelamento de consultas

---
+++ JomedAPIForms/Models/MotivoDeCancelamento.cs
@@ -0,0 +1,15 @@
+﻿using jomedAPI.Models.Enum;
+using System.Text.Json.Serialization;
+
+namespace JomedAPIForms.Models;
+
+public class MotivoDeCancelamento
+{
+    public MotivoDeCancelamento(MotivoCancelamento motivo)
+    {
+        motivoCancelamento = motivo;
+    }
+
+    [JsonPropertyName("motivoCancelamento")]
+    public MotivoCancelamento motivoCancelamento { get; set; }
+}
